e78422dbc5b493c9e3f22654ea163d6b76537f60
InfiniteXyy <xuyiyangwing@gmail.com>
2022-02-21 14:27:38 +0800

    chore: add react-tracked example

--- example/with-selector/index.tsx
+++ example/with-selector/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, memo } from 'react';
+import { createTrackedSelector } from 'react-tracked';
 import { defineModule } from '../../src';
 
 const CounterModule = defineModule({ count: 0, input: 'hello' })
@@ -17,7 +18,7 @@ export const WithSelector: FC = memo(() => {
       <p>
         count: <b style={{ marginRight: 20 }}>{count}</b> input: <b>{input}</b>
       </p>
-      <button onClick={add}>+1</button>
+      <CounterComponent />
       <InputComponent />
     </div>
   );
@@ -27,4 +28,12 @@ const InputComponent: FC = memo(() => {
   const [input, { setInput }] = CounterModule.use((state) => state.input);
   console.log('input component rerender');
   return <input value={input} onChange={(e) => setInput(e.target.value)} />;
+});
+
+const useCounterModuleState = createTrackedSelector(CounterModule.useState);
+const CounterComponent: FC = memo(() => {
+  const { add } = CounterModule.useActions();
+  const { count } = useCounterModuleState();
+  console.log('counter component rerender');
+  return <button onClick={add}>{count}</button>;
 });
